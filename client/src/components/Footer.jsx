2521674bb192234546680f5bfcafedff3c289354
InshiyaRavat <insravat@gmail.com>
2024-02-09 20:36:27 +0530

    useLocation used in footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 const Footer = () => {
+  // const location=useLocation();
+  // if(location.pathname!='/loading'){
   return (
     <footer>
         &copy; BookNook | 2024
     </footer>
-  )
+  );
+  // }
+  // return null;
 }
 
 export default Footer
