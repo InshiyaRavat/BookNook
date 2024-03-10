1f95b77f4236ea1c473be2fd7047b082be48f255
InshiyaRavat <insravat@gmail.com>
2024-03-10 17:48:03 +0530

    replaced api

--- client/src/components/Store.jsx
+++ client/src/components/Store.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import Loading from '../components/Loading';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Navbar from './Navbar';
 
 const Store = () => {
+  const authenticated = sessionStorage.getItem('authentication');
+  const navigate = useNavigate();
+
   const [search, setSearch] = useState('');
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -11,9 +14,9 @@ const Store = () => {
   async function fetchBooks() {
     setLoading(true);
     try {
-      let url = "http://localhost:3000/AllBooks";
+      let url = "http://localhost:8085/book";
       if (search !== '') {
-        url = `http://localhost:3000/AllBooks?category=${search}`;
+        url = `http://localhost:8085/book/search?category=${search}`;
       }
       const response = await fetch(url);
       if (!response.ok) {
@@ -28,6 +31,7 @@ const Store = () => {
       setLoading(false);
     }
   }
+
   useEffect(() => {
     fetchBooks();
   }, []);
@@ -38,52 +42,57 @@ const Store = () => {
   };
 
   return (
-    <div>
-      <Navbar/>
-      {loading ? (
-        <Loading />
-      ) : (
-        <div className="row g-4">
-          <form onSubmit={handleSubmit} className="d-flex justify-content-end" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-            />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
-          {books.map((book, index) => (
-            <Link
-              to={`/product/${book.title}`}
-              state={book}
-              style={{ textDecoration: 'none' }}
-            >
-              <div key={index} className="mycard card mb-3">
-                <div className="row g-0">
-                  <div className="col-md-4">
-                    <img src={book.imageUrl} className="imgCard card-img-top" alt={book.title} style={{ maxWidth: '100px !important', height: '200px !important' }} />
-                  </div>
-                  <div className="col-md-8">
-                    <div className="card-body">
-                      <h5 className="mycardtitle card-title">{book.title}</h5>
-                      <p className="cardsubtitle card-text">{book.description}</p>
-                      <small className="cardprice text-body-secondary">
-                        <span>$</span> {book.price}
-                      </small>
+    <>
+      {authenticated === "success" ? (
+        <div>
+          <Navbar/>
+          {loading ? (
+            <Loading />
+          ) : (
+            <div className="row g-4">
+              <form onSubmit={handleSubmit} className="d-flex justify-content-end" role="search">
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
+                />
+                <button className="btn btn-outline-success" type="submit">
+                  Search
+                </button>
+              </form>
+              {books.map((book, index) => (
+                <Link
+                  to={`/product/${book.title}`}
+                  state={book}
+                  style={{ textDecoration: 'none' }}
+                  key={index}
+                >
+                  <div className="mycard card mb-3">
+                    <div className="row g-0">
+                      <div className="col-md-4">
+                        <img src={book.imageUrl} className="imgCard card-img-top" alt={book.title} style={{ maxWidth: '100px !important', height: '200px !important' }} />
+                      </div>
+                      <div className="col-md-8">
+                        <div className="card-body">
+                          <h5 className="mycardtitle card-title">{book.title}</h5>
+                          <p className="cardsubtitle card-text">{book.description}</p>
+                          <small className="cardprice text-body-secondary">
+                            <span>$</span> {book.price}
+                          </small>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </div>
-            </Link>
-          ))}
+                </Link>
+              ))}
+            </div>
+          )}
         </div>
-      )}
-    </div>
+      ) : navigate('/')}
+    </>
   );
 };
 
